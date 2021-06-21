0d1e0d611d885fa532ee4687d807324bd1b34385
Denise Castro <dmaria.natal@gmail.com>
2021-06-20 21:08:41 -0300

    Testando o REST

---
+++ src/main/java/br/com/denisemaria/SistemaWebLojaApplication.java
@@ -0,0 +1,13 @@
+package br.com.denisemaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaWebLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaWebLojaApplication.class, args);
+	}
+
+}
