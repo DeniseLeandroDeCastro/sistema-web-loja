5b0f054aa177573e257e99e10e10f5671caad780
Denise Castro <dmaria.natal@gmail.com>
2021-06-24 20:19:35 -0300

    Criando service para categoria

---
+++ src/main/java/br/com/denisemaria/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package br.com.denisemaria.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.denisemaria.domain.Categoria;
+import br.com.denisemaria.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+
+}
