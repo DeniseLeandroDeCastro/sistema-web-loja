bb2c4ad2c5e32426a7ce6bce7eae24a1b9950b1a
Denise Castro <dmaria.natal@gmail.com>
2021-06-25 21:30:18 -0300

    Operação de instanciação

--- src/main/java/br/com/denisemaria/SistemaWebLojaApplication.java
+++ src/main/java/br/com/denisemaria/SistemaWebLojaApplication.java
@@ -1,13 +1,33 @@
 package br.com.denisemaria;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.denisemaria.domain.Categoria;
+import br.com.denisemaria.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SistemaWebLojaApplication {
+public class SistemaWebLojaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaWebLojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
