dc56505825a02aa35e866dfa71f874c3214f9f41
Denise Castro <dmaria.natal@gmail.com>
2021-06-27 16:49:09 -0300

    Ajustes no endpoint /categorias{id}

--- src/main/java/br/com/denisemaria/services/CategoriaService.java
+++ src/main/java/br/com/denisemaria/services/CategoriaService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import br.com.denisemaria.domain.Categoria;
 import br.com.denisemaria.repositories.CategoriaRepository;
+import br.com.denisemaria.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -14,10 +16,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria find(Integer id) {
+	public Categoria find(Integer id) throws ObjectNotFoundException {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));	
 	}
 
 }
